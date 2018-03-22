9bdfac2a3853c4fe717d11c2db0eab7ff23e9da5
nanter1986 <nanter1986@gmail.com>
2018-03-22 10:18:14 +0200

    replace

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
   $name="I'm Nanter";
   $separate=" & ";
   $name2="I'm Alina";
-  echo $name . $separate . $name2;
+  $longString=$name . $separate . $name2;
+  $replace=str_replace("I'm","Here is",$longString);
+  echo $replace;
 ?>
