592cc5f44d6b3035b52e32b81be2a265bc8cdad9
nanter1986 <nanter1986@gmail.com>
2018-03-23 10:10:08 +0200

    upper case words

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
   $name="I'm Nanter and I mean it!";
   $separate=" & ";
-  $name2="I'm Alina";
-  $longString=substr($name,11,5) . $separate . $name2;
+  $name2="i'm alina";
+  $longString=substr($name,11,5) . $separate . ucwords($name2);
   $replace=str_replace("I'm","Here is",$longString);
   echo $replace;
 ?>
