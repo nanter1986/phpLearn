f004dc946252c63b2fbae38b1281e9807cd150be
nanter1986 <nanter1986@gmail.com>
2018-03-23 10:13:13 +0200

    string to lower

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
   $name2="i'm alina";
   $longString=substr($name,11,5) . $separate . ucwords($name2);
   $replace=str_replace("I'm","Here is",$longString);
-  echo strtoupper($replace);
+  echo strtolower($replace);
 ?>
