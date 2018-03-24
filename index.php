a6e01a108e329fe4afd8ab3f9c9e2fc939708f63
nanter1986 <nanter1986@gmail.com>
2018-03-24 10:15:12 +0200

    assosiative arrays

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-  $myArray=array("I'm Nanter and I mean it!"," & ","i'm alina");
-  $name="I'm Nanter and I mean it!";
-  $separate=" & ";
-  $name2="i'm alina";
-  $longString=substr($myArray[0],11,5) . $separate . ucwords($myArray[2]);
+  $myArray=array("me"=>"I'm Nanter and I mean it!","sep"=>" & ","her"=>"i'm alina");
+  $longString=substr($myArray["me"],11,5) . $myArray["sep"] . ucwords($myArray["her"]);
   $replace=str_replace("I'm","Here is",$longString);
   echo strtolower($replace);
   #######################
