379453983b12823f07f2a362747e26eec9cb0db1
nanter1986 <nanter1986@gmail.com>
2018-03-27 12:01:30 +0300

    implode and explode

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-  $myArray=array("me"=>"I'm Nanter and I mean it!","sep"=>" & ","her"=>"i'm alina");
-  $longString=substr($myArray["me"],11,5) . $myArray["sep"] . ucwords($myArray["her"]);
-  $replace=str_replace("I'm","Here is",$longString);
-  echo strtolower($replace);
-  #######################
-  $theElement=htmlentities("<font>");
-  echo "Element<font> is &rarr; " . $theElement;
+  $myText="Nanter and Alina!";
+  $explodedText=explode(" ",$myText,3);
+  foreach($explodedText as $e){
+    echo $e . "\n";
+  }
+  $implodedText=implode(", ",$explodedText);
+  echo $implodedText;
 ?>
