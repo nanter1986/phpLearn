befadacb5ee7c2df1d98b5df6db7d759a897f6b8
nanter1986 <nanter1986@gmail.com>
2018-03-30 09:30:59 +0300

    tried url encode

--- index.php
+++ index.php
@@ -2,6 +2,5 @@
   include "includeMe2.php";
   $aVar=file_get_contents("includeMe2.html");
   echo "$aVar \n";
-  echo "$aVar \n";
 
 ?>
--- newPage.php
+++ newPage.php
@@ -1,3 +1,5 @@
 <?php
-  echo $_GET["firstname"];
+  echo urlencode("your first name " . $_GET["firstname"] . "\n");
+  echo "your last name " . $_GET["lastname"] . "\n";
+  echo "your age " . $_GET["age"] . "\n";
 ?>
