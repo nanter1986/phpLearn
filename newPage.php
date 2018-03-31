86043a9e1dd4641db33666f223f32796bde1ec39
nanter1986 <nanter1986@gmail.com>
2018-03-31 13:23:57 +0300

    nl2br

--- newPage.php
+++ newPage.php
@@ -1,8 +1,8 @@
 <?php
   if(isset($_POST['firstname'])){
-    echo "your first name " . $_POST["firstname"] . "\n";
-    echo "your last name " . $_POST["lastname"] . "\n";
-    echo "your age " . $_POST["age"] . "\n";
+    echo nl2br("your first name " . $_POST["firstname"] . "\n");
+    echo nl2br("your last name " . $_POST["lastname"] . "\n");
+    echo nl2br("your age " . $_POST["age"] . "\n");
   }
 
 ?>
