a45cc26638743cf315fbfb139d9a1bdd2a20a030
nanter1986 <nanter1986@gmail.com>
2018-04-02 09:38:10 +0300

    doctype

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-  include "includeMe2.php";
+  include "includeMe.php";
   $aVar=file_get_contents("includeMe2.html");
   echo "$aVar \n";
-  $now= time();
+  $now= mktime(23,0,0,7,13,1986);
   echo date("F j, y, g:i a" ,$now);
 
 ?>
