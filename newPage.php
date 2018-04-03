10aa1cd2795797f1b547b087122455d70f85dfc9
nanter1986 <nanter1986@gmail.com>
2018-04-03 09:31:39 +0300

    short if statement

--- newPage.php
+++ newPage.php
@@ -2,7 +2,8 @@
   if(isset($_POST['firstname'])){
     echo nl2br("your first name " . $_POST["firstname"] . "\n");
     echo nl2br("your last name " . $_POST["lastname"] . "\n");
-    echo nl2br("your age " . $_POST["age"] . "\n");
+    $youth=($_POST['age']<30?'young':'old');
+    echo nl2br("you are  " . $youth . "\n");
   }
 
 ?>
