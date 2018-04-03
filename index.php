be77594f879625b549afe94e5bff8137e8a71e26
nanter1986 <nanter1986@gmail.com>
2018-04-03 10:08:10 +0300

    sessions

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
+  if(!isset($_SESSION)){
+    session_start();
+    echo 'session started';
+  }
+
   include "includeMe.php";
   $aVar=file_get_contents("includeMe2.html");
   echo "$aVar \n";
   $now= mktime(23,0,0,7,13,1986);
   echo date("F j, y, g:i a" ,$now);
+  $_SESSION['ability']='im awesome';
+  $_SESSION['weakness']='cereal';
 
 ?>
--- newPage.php
+++ newPage.php
@@ -1,9 +1,28 @@
 <?php
+  if(!isset($_SESSION)){
+    session_start();
+    echo "session started\n";
+  }
   if(isset($_POST['firstname'])){
     echo nl2br("your first name " . $_POST["firstname"] . "\n");
     echo nl2br("your last name " . $_POST["lastname"] . "\n");
     $youth=($_POST['age']<30?'young':'old');
     echo nl2br("you are  " . $youth . "\n");
+    foreach($_SESSION as $s => $v){
+      echo nl2br("\nyou $_SESSION[$s]");
+    }
   }
+
+  if(isset($_SESSION)){
+    session_destroy();
+    echo nl2br("\nsession destroyed\n");
+  }
+
+  echo "after destroy? " . $_SESSION['ability'];
+  foreach($_SESSION as $s => $v){
+    echo nl2br("\nunsetting $_SESSION[$s]");
+    unset($_SESSION[$s]);
+  }
+
 
 ?>
