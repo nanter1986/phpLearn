effa5642ecb32bcdd6bd5396885fb08c6d2b40e8
nanter1986 <nanter1986@gmail.com>
2018-04-13 09:14:55 +0300

    started log in system

--- login_confirm.php
+++ login_confirm.php
@@ -4,6 +4,20 @@ if ($conn->connect_error) {
     echo "ERROR";
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "DONE!";
+    echo "DONE!<br>";
 }
+$name=$_POST['username'];
+$pass=$_POST['password'];
+$query="SELECT * FROM users WHERE username='$name'";
+$result=mysqli_query($conn,$query);
+$array1=mysqli_fetch_array($result,MYSQLI_ASSOC);
+if($pass==$array1['password'] && $name==$array1['username']){
+  echo "Log in successful!";
+}else if($name==$array1['username']){
+  echo "incorrect password";
+}else{
+  echo "user doesnt exists";
+}
+echo "$name" . "/" . $array1['username'];
+echo "$pass" . "/" . $array1['password'];
  ?>
