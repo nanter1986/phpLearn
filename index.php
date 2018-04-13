82e138e012f15423b0c96f7a832a9de5c4050a85
nanter1986 <nanter1986@gmail.com>
2018-04-13 08:24:04 +0300

    moved links to navbar

--- index.php
+++ index.php
@@ -36,8 +36,7 @@
     <![endif]-->
   </head>
   <body>
-    <a href="addProduct.php">add product</a>
-    <a href="viewAllProducts.php">view all products</a>
+    
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
