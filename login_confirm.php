9a0d5b8d9ee09c8bcd0e5c2059815b96bd0f0dcd
nanter1986 <nanter1986@gmail.com>
2018-04-16 09:18:14 +0300

    setting cookie login

--- login_confirm.php
+++ login_confirm.php
@@ -12,8 +12,10 @@ $query="SELECT * FROM users WHERE username='$name'";
 $result=mysqli_query($conn,$query);
 $array1=mysqli_fetch_array($result,MYSQLI_ASSOC);
 if($pass==$array1['password'] && $name==$array1['username'] && $name=="nanter1986"){
+  setcookie("userAth" , "admin", time() +86400*30, "/" );
   echo "Log in successfully as Admin.You can add and remove products!";
 }else if($pass==$array1['password'] && $name==$array1['username']){
+  setcookie("userAth" , "user", time() +86400*30, "/");
   echo "Log in successful!";
 }else if($name==$array1['username']){
   echo "incorrect password";
@@ -22,4 +24,5 @@ if($pass==$array1['password'] && $name==$array1['username'] && $name=="nanter198
 }
 echo "$name" . "/" . $array1['username'];
 echo "$pass" . "/" . $array1['password'];
+echo "<a href='index.php'>back to home</a>"
  ?>
